0572a773a50beff974c01c3b419ef714a45b35de
unknown <jayprakashpj2525@gmail.com>
2023-06-25 21:14:58 +0530

    jay

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { collection } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../Firebase";
 import { CiLocationOn } from "react-icons/ci";
 import { useCallback, useEffect, useState } from "react";
@@ -6,21 +6,43 @@ import { Link } from "react-router-dom";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { BiLoaderAlt } from "react-icons/bi";
 export default function Card() {
-  const docref = collection(db, "Live Tailors");
-  const [docs, loading, error] = useCollectionData(docref);
-  const [data, setdata] = useState();
+
+
+  const [profileData, setProfileData] = useState([]);
+
+  const userLocation = "Hyderabad";
+  useEffect(() => {
+    const fetchFitleredData = async () => {
+      const profileRef = collection(db, 'PROFILELIVE');
+      // filter the profile based on address 
+      const filteredProfiles = query(profileRef, where("Address", "==", userLocation))
+      const profileSnapshot = await getDocs(filteredProfiles);
+
+      const data = profileSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+
+      setProfileData(data);
+    };
+
+    fetchFitleredData();
+  },[userLocation]);
+
+  // const docref = collection(db, "PROFILELIVE");
+  // const [data, setdata] = useState();
 
   // Fetching the data
-  const FetchTailors = useCallback(() => {
-    if (error) {
-      console.log(error);
-    }
-    setdata(docs);
-  }, [docs, error]);
+  // const FetchTailors = useCallback(() => {
+  //   if (error) {
+  //     console.log(error);
+  //   }
+  //   setdata(docs);
+  // }, [docs, error]);
 
-  useEffect(() => {
-    FetchTailors();
-  }, [FetchTailors]);
+  // useEffect(() => {
+  //   FetchTailors();
+  // }, [FetchTailors]);
   //  Filter with User's Location
   // console.log(
   //   docs?.map((item, index) => {
@@ -31,16 +53,8 @@ export default function Card() {
   // );
   return (
     <main className="px-8 my-14  flex justify-center flex-col gap-5 items-center sm:grid sm:grid-cols-2 md:grid md:grid-cols-3 lg:grid lg:grid-cols-4 sm:gap-6">
-      {loading ? (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <BiLoaderAlt
-            size={40}
-            className="animate-spin duration-300 ease-in-out"
-            color="yellow"
-          />
-        </div>
-      ) : (
-        data?.map((_, index) => {
+     
+       { profileData?.map((_, index) => {
           return (
             <>
               <div
@@ -51,13 +65,13 @@ export default function Card() {
                   <img src={_.image} alt="pic" className="rounded-lg" />
                   <div className="space-y-2.5 my-3.5 pl-1">
                     <div className="flex justify-between items-center">
-                      <h1 className="text-2xl font-semibold">{_.Name}</h1>
+                      <h1 className="text-2xl font-semibold">{_.ShopName}</h1>
                       <div className="flex items-center space-x-2">
                         <CiLocationOn size={24} color="grey" />
                         <p className="text-sm text-slate-600">{_.Address}</p>
                       </div>
                     </div>
-                    <p className="leading-7 text-sm">{_.About}</p>
+                    <p className="leading-7 text-sm">{_.AboutShop}</p>
                   </div>
                   <div className="flex items-center justify-end space-x-4 py-3.5">
                     <Link to={_.UserJwt}>
@@ -73,8 +87,7 @@ export default function Card() {
               </div>
             </>
           );
-        })
-      )}
+       })}
     </main>
   );
 }
--- src/models/AddProfile.js
+++ src/models/AddProfile.js
@@ -1,12 +1,95 @@
 import { useState } from "react";
 import { RxCross2 } from "react-icons/rx";
+import {getDownloadURL, getStorage, ref, uploadBytes} from 'firebase/storage'
+import { db } from "../Firebase";
+import {collection, addDoc} from 'firebase/firestore';
 export default function AddProfile({ tooglemodel, settooglemodel }) {
+  
+
+  const storage = getStorage();
+
+  // getting user location ....
+
+  // postion 
+  // const [location, setLocation] = useState();
+  // const Address = async(position) => {
+  //   const {latitude, longitude} = position.coords;
+  //   try {
+
+  //     const userAddress = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}`;
+  //     const response = await fetch(userAddress);
+  //     const data = await response.json();
+  //     const {suburb} = data.address;
+  //     // setLocation(suburb);
+  //     console.log(suburb);
+      
+  //   } catch (error) {
+  //     console.log(error);
+  //   } 
+
+  // }
+
+  // navigator.geolocation.getCurrentPosition(Address, (error) => {console.log(error)})
+  const [image, setImage] = useState(null);
   const [profileCard, setprofileCard] = useState({
-    image: "",
+    image: null,
+    imageURL: "",
+    Address: "Hyderabad",
     ShopName: "",
     AboutShop: "",
     Contact: "",
   });
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+    const imageUrl = URL.createObjectURL(file);
+    setprofileCard({
+      ...profileCard,
+      image: file,
+      imageURL: imageUrl,
+    });
+  };
+
+  
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(db, "PROFILELIVE"), profileCard);
+  
+      // Upload the image to Firebase Storage
+      if (image) {
+        const storageRef = ref(storage, `images/${image.name}`);
+        const snapshot = await uploadBytes(storageRef, image);
+        const downloadUrl = await getDownloadURL(snapshot.ref);
+  
+        // Update the profile card with the image URL
+        setprofileCard({
+          ...profileCard,
+          image: downloadUrl,
+        });
+      }
+  
+      // Reset the form fields
+      setprofileCard({
+        image: null,
+        imageURL: "",
+        Address: "",
+        ShopName: "",
+        AboutShop: "",
+        Contact: "",
+      });
+      settooglemodel(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+  
 
   return (
     <>
@@ -28,14 +111,31 @@ export default function AddProfile({ tooglemodel, settooglemodel }) {
                 Make Your Profile Live in Mins
               </h1>
             </div>
-            <form className="flex flex-col justify-center items-center space-y-8">
+            <form className="flex flex-col justify-center items-center space-y-8" onSubmit={handleSubmit}>
               <div className="">
                 <input
-                  type="text"
+                  type="file"
                   placeholder="Image"
                   required
                   value={profileCard.image}
                   className="bg-slate-100 px-8 outline-none py-2.5"
+                  onChange={handleImageUpload}
+              //     onChange={async (e) => {
+              //       const file = e.target.files[0];
+              //      const imageUrl = URL.createObjectURL(file);
+
+              //        const storageRef = ref(storage, `images/${file.name}`);
+              //      const snapshot = await uploadBytes(storageRef, file);
+              //             const downloadUrl = await getDownloadURL(snapshot.ref);
+
+              //          setImage(file);
+              // //  setImageUrl(downloadUrl);
+              //        setprofileCard({
+              //          ...profileCard,
+              //         image: file,
+              //       imageUrl: downloadUrl,
+              //          });
+              //     }}
                 />
               </div>
               <div>
@@ -45,15 +145,27 @@ export default function AddProfile({ tooglemodel, settooglemodel }) {
                   required
                   value={profileCard.ShopName}
                   className="bg-slate-100 px-8 outline-none py-2.5"
+                  onChange={(e) => {
+                    setprofileCard({
+                      ...profileCard,
+                      ShopName: e.target.value,
+                    })
+                  }}
                 />
               </div>
               <div>
                 <input
-                  type="text"
+                  type="number"
                   placeholder="Contact Number"
                   required
                   value={profileCard.Contact}
                   className="bg-slate-100 px-8 outline-none py-2.5"
+                  onChange={(e) => {
+                    setprofileCard({
+                      ...profileCard,
+                      Contact: e.target.value,
+                    })
+                  }}
                 />
               </div>
               <div>
@@ -63,9 +175,15 @@ export default function AddProfile({ tooglemodel, settooglemodel }) {
                   placeholder="About Shop"
                   required
                   className="bg-slate-100 px-8 outline-none py-2.5"
+                  onChange={(e) => {
+                    setprofileCard({
+                      ...profileCard,
+                      AboutShop: e.target.value,
+                    })
+                  }}
                 />
               </div>
-              <button className="bg-yellow-500 text-white px-20 py-1.5 rounded-full">
+              <button className="bg-yellow-500 text-white px-20 py-1.5 rounded-full" type="submit">
                 Done
               </button>
             </form>
